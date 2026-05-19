2666d73c111a2d69bf81ee4928c54618b84c8d0b
TheCotOfTehas <TsebroEvgeny@yandex.ru>
2026-05-19 20:55:27 +0300

    add command HLT

--- cpu.c
+++ cpu.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
 #include <stdint.h>
+#include <stdbool.h>
 
 void HLT();
 void ADD(uint8_t *, uint8_t *);
@@ -7,6 +8,7 @@ void SUB(uint8_t *, uint8_t *);
 void MOV(uint8_t *, uint8_t );
 void IR();
 void readerCommand(void);
+bool stop = false; //влаг для остановки программы
 uint8_t ram[4];
 uint8_t cpu[5];
 
@@ -14,12 +16,15 @@ int main()
 {
     return 0;
 }
+
 void readerCommand(void)
 {
     int command;
     scanf("%d",&command);
     switch (command) 
     {
+        case 0: HLT(); 
+            break;
         case 1: ADD(&ram[0], &ram[1]); 
             break;
         case 2: SUB(&ram[0], &ram[1]); 
@@ -29,6 +34,10 @@ void readerCommand(void)
         case 4: IR();                
             break;
     }
+}
+void HLT()
+{
+    stop = true;
 }
 
 void IR()
@@ -53,10 +62,3 @@ void MOV(uint8_t * a, uint8_t b)
 {
     *a = b;
 }
-/*
-    0 	HLT 	нет Конец программы, стоп (halt)
-    1 	ADD 	reg1 reg2 	Сложить числа из регистров reg1 и reg2, результат записать в reg1
-    2 	SUB 	reg1 reg2 	Вычесть числа из регистров reg1 и reg2, результат записать в reg1
-    3 	MOV 	reg число 	Записать число в регистр reg
-    4 	IR 	нет 	напечатать числа из всех регистров через пробел
-*/
