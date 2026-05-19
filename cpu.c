56aca816574d247543f0c28be591f8bbc54671d5
TheCotOfTehas <TsebroEvgeny@yandex.ru>
2026-05-19 20:42:51 +0300

    add readerCommand

--- cpu.c
+++ cpu.c
@@ -6,17 +6,29 @@ void ADD(uint8_t *, uint8_t *);
 void SUB(uint8_t *, uint8_t *);
 void MOV(uint8_t *, uint8_t );
 void IR();
+void readerCommand(void);
 uint8_t ram[4];
+uint8_t cpu[5];
 
 int main() 
 {
-    uint8_t * A = &ram[0];
-    uint8_t * B = &ram[1];
-    uint8_t * C = &ram[2];
-    uint8_t * D = &ram[3];
-    uint8_t cur;
-    scanf("%d",&cur);
     return 0;
+}
+void readerCommand(void)
+{
+    int command;
+    scanf("%d",&command);
+    switch (command) 
+    {
+        case 1: ADD(&ram[0], &ram[1]); 
+            break;
+        case 2: SUB(&ram[0], &ram[1]); 
+            break;
+        case 3: MOV(&ram[0], 42);    
+            break;
+        case 4: IR();                
+            break;
+    }
 }
 
 void IR()
