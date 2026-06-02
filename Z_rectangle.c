8a38fa642c6950fabb8e0faca2750323540346b9
TheCotOfTehas <TsebroEvgeny@yandex.ru>
2026-06-02 07:34:31 +0300

    add read and print array

--- Z_rectangle.c
+++ Z_rectangle.c
@@ -1,7 +1,39 @@
 #include <stdio.h>
 #include <stdbool.h>
 
+void ReadPlan(int size, char matrix[size][size]);
+void PrintPlan(int size, char matrix[size][size]);
+
 int main()
 {
+    int size =0;
+    scanf("%d", &size);
+    char matrix[size][size];
+    ReadPlan(size, matrix);
+    PrintPlan(size, matrix);
+}
 
+void ReadPlan(int size, char matrix[size][size])
+{
+    char temp;
+
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+        {
+            scanf(" %c", &temp);
+            matrix[i][j] = temp;
+        }    
+    }
+}
+
+void PrintPlan(int size, char matrix[size][size])
+{
+    for (int i = 0; i < size; i++)
+    {
+        for (int j = 0; j < size; j++)
+            printf("%c", matrix[i][j]);
+  
+        printf("\n");
+    }
 }
